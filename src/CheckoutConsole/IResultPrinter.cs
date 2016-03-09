eb1812b9d836f85d6225f28a1f9b0a5a7f962366
Gavin Zhu <heavenwing@msn.com>
2016-03-10 00:34:24 +0800

    定义前端结果打印接口。

---
+++ src/CheckoutConsole/IResultPrinter.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using AdminConsole.Dtos;
+using Newtonsoft.Json;
+
+namespace CheckoutConsole
+{
+    public interface IResultPrinter
+    {
+        void Print(ComputeResultDto result,TextWriter writer);
+    }
+
+    public class RawResultPrinter : IResultPrinter
+    {
+        public void Print(ComputeResultDto result, TextWriter writer)
+        {
+            writer.Write(JsonConvert.SerializeObject(result));
+        }
+    }
+}
--- src/CheckoutConsole/MainForm.cs
+++ src/CheckoutConsole/MainForm.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using AdminConsole.Dtos;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
@@ -22,13 +24,24 @@ namespace CheckoutConsole
 
         private async void btnCheckout_Click(object sender, EventArgs e)
         {
-            var data = JsonConvert.DeserializeObject<string[]>(txtInput.Text);
+            var output = await GetComputingResultFromServer();
 
+            var writer = new StringWriter();
+            if (Printer == null)
+                Printer = new RawResultPrinter();
+            Printer.Print(output,writer);
+            txtOutput.Text = writer.ToString();
+        }
+
+        public IResultPrinter Printer { get; set; }
+
+        private async Task<ComputeResultDto> GetComputingResultFromServer()
+        {
+            var data = JsonConvert.DeserializeObject<string[]>(txtInput.Text);
             var api = new HttpClient();
             api.BaseAddress = new Uri("http://localhost:61852/");
             var response = await api.PostAsJsonAsync("http://localhost:61852/api/ComputeApi", data);
-            var output = await response.Content.ReadAsStringAsync();
-            txtOutput.Text = output;
+            return await response.Content.ReadAsAsync<ComputeResultDto>();
         }
     }
 }
